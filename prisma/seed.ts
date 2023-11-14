9eaa707d8d066ba55f96703a17ecbfa4c9b5a185
jmatondo <j_matondo@hotmail.com>
2023-11-14 10:29:56 +0100

    First Commit

---
+++ prisma/seed.ts
@@ -0,0 +1,27 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.upsert({
+    where: {
+      email: "test@gmail.com",
+    },
+    update: {},
+    create: {
+      email: "test@gmail.com",
+      password: "test",
+      name: "",
+    },
+  });
+
+  console.log({ user });
+}
+
+main()
+  .then(() => prisma.$disconnect())
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
